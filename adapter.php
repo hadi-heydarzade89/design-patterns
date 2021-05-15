f7c370bc46e7f4ff121d6c6eb749fd3df22a216b
Hadi Heydarzade <hadi.heydarzade@protonmail.com>
2021-05-16 00:26:29 +0430

    Add adapter design pattern

---
+++ adapter.php
@@ -0,0 +1,86 @@
+<?php
+
+interface Notification
+{
+    public function send(string $title, string $message);
+}
+
+class EmailNotification implements Notification
+{
+    private $adminEmail;
+
+    public function __construct(string $adminEmail)
+    {
+        $this->adminEmail = $adminEmail;
+    }
+
+    public function send(string $title, string $message)
+    {
+        mail($this->adminEmail, $title, $message);
+        echo "Sent email with title '$title' to '{$this->adminEmail}' that says '$message'.";
+    }
+}
+
+
+class SlackApi
+{
+    private $login;
+    private $apiKey;
+
+    public function __construct(string $login, string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+        $this->login = $login;
+    }
+
+    public function logIn(): void
+    {
+        // Send authentication request to Slack web service.
+        echo "Logged in to a slack account '{$this->login}'.\n";
+    }
+
+    public function sendMessage(string $chatId, string $message): void
+    {
+        // Send message post request to Slack web service.
+        echo "Posted following message into the '$chatId' chat: '$message'.\n";
+    }
+}
+
+class SlackNotification implements Notification
+{
+    private $slack;
+    private $chatId;
+
+    public function __construct(SlackApi $slack, string $chatId)
+    {
+        $this->chatId = $chatId;
+        $this->slack = $slack;
+    }
+
+    public function send(string $title, string $message): void
+    {
+        $slackMessage = "#" . $title . "# " . strip_tags($message);
+        $this->slack->logIn();
+        $this->slack->sendMessage($this->chatId, $slackMessage);
+    }
+}
+
+
+function clientCode(Notification $notification)
+{
+    echo $notification->send("Website is down!",
+        "<strong style='color:red;font-size: 50px;'>Alert!</strong> " .
+        "Our website is not responding. Call admins and bring it up!");
+
+}
+
+
+echo "Client code is designed correctly and works with email notifications:\n";
+$notification = new EmailNotification("developers@example.com");
+clientCode($notification);
+echo "\n\n";
+
+echo "The same client code can work with other classes via adapter:\n";
+$slackApi = new SlackApi("example.com", "XXXXXXXX");
+$notification = new SlackNotification($slackApi, "Example.com Developers");
+clientCode($notification);
