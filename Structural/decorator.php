babcbda2d0b2a16dd96577c574e1d79d8e7f24be
Hadi Heydarzade <H.Heydarzade@NBSAfzar.Local>
2021-06-16 15:49:44 +0430

    Add decorator design patterns

---
+++ Structural/decorator.php
@@ -0,0 +1,159 @@
+<?php
+
+
+interface InputFormat
+{
+    public function formatText(string $text): string;
+}
+
+class TextInput implements InputFormat
+{
+    public function formatText(string $text): string
+    {
+        return $text;
+    }
+}
+
+
+class TextFormat implements InputFormat
+{
+    protected $inputFormat;
+
+    public function __construct(InputFormat $inputFormat)
+    {
+        $this->inputFormat = $this->inputFormat;
+    }
+
+    public function formatText(string $text): string
+    {
+        return $this->inputFormat->formatText($text);
+    }
+}
+
+class PlainTextFilter extends TextFormat
+{
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+        return strip_tags($text);
+    }
+}
+
+class DangerousHTMLTagsFilter extends TextFormat
+{
+    private $dangerousTagPatterns = [
+        "|<script.*?>([\s\S]*)?</script>|i", // ...
+    ];
+
+
+    private $dangerousAttributes = [
+        "onclick", "onkeypress", // ...
+    ];
+
+
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+
+        foreach ($this->dangerousAttributes as $attribute) {
+            $text = preg_replace_callback('|<(.*?)>|', function ($matches) use ($attribute) {
+                $result = preg_replace("|$attribute=|i", '', $matches[1]);
+                return "<" . $result . ">";
+            }, $text);
+        }
+
+        return $text;
+    }
+}
+
+
+class MarkdownFormat extends TextFormat
+{
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+
+        $chunks = preg_split('|\n\n|', $text);
+
+        foreach ($chunks as &$chunk) {
+            if (preg_match('|^#+|', $chunks)) {
+                $chunk = preg_replace_callback('|^(#+)(.*?)$|', function ($matches) {
+                    $h = strlen($matches[1]);
+                    return "<h$h>" . trim($matches[2]) . "</h$h>";
+                }, $chunk);
+            } else {
+                $chunk = "<p>$chunk</p>";
+            }
+        }
+        $text = implode("\n\n", $chunks);
+
+
+        $text = preg_replace("|__(.*?)__|", '<strong>$1</strong>', $text);
+        $text = preg_replace("|\*\*(.*?)\*\*|", '<strong>$1</strong>', $text);
+        $text = preg_replace("|_(.*?)_|", '<em>$1</em>', $text);
+        $text = preg_replace("|\*(.*?)\*|", '<em>$1</em>', $text);
+
+        return $text;
+    }
+}
+
+
+function displayCommentAsAWebsite(InputFormat $format, string $text)
+{
+    echo $format->formatText($text);
+}
+
+$dangerousComment = <<<HERE
+Hello! Nice blog post!
+Please visit my <a href='http://www.iwillhackyou.com'>homepage</a>.
+<script src="http://www.iwillhackyou.com/script.js">
+  performXSSAttack();
+</script>
+HERE;
+
+$nativeInput = new TextInput();
+echo "Website renders comments without filtering (unsafe):\n";
+displayCommentAsAWebsite($nativeInput,$dangerousComment);
+
+echo "\n\n\n";
+
+
+
+$filteredInput = new PlainTextFilter($nativeInput);
+echo "Website renders comments after stripping all tags (safe):\n";
+displayCommentAsAWebsite($filteredInput, $dangerousComment);
+echo "\n\n\n";
+
+
+/**
+ * Decorator allows stacking multiple input formats to get fine-grained control
+ * over the rendered content.
+ */
+$dangerousForumPost = <<<HERE
+# Welcome
+
+This is my first post on this **gorgeous** forum.
+
+<script src="http://www.iwillhackyou.com/script.js">
+  performXSSAttack();
+</script>
+HERE;
+
+/**
+ * Naive post rendering (unsafe, no formatting).
+ */
+$naiveInput = new TextInput();
+echo "Website renders a forum post without filtering and formatting (unsafe, ugly):\n";
+displayCommentAsAWebsite($naiveInput, $dangerousForumPost);
+echo "\n\n\n";
+
+/**
+ * Markdown formatter + filtering dangerous tags (safe, pretty).
+ */
+$text = new TextInput();
+$markdown = new MarkdownFormat($text);
+$filteredInput = new DangerousHTMLTagsFilter($markdown);
+echo "Website renders a forum post after translating markdown markup" .
+    " and filtering some dangerous HTML tags and attributes (safe, pretty):\n";
+displayCommentAsAWebsite($filteredInput, $dangerousForumPost);
+echo "\n\n\n";
